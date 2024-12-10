27c952dc98edf4d69fd6bee06aa35a77ca064722
samChisem29 <tonyfhg1000@gmail.com>
2024-12-10 22:21:25 +0300

    CHANGING

--- API/main.py
+++ API/main.py
@@ -2,10 +2,14 @@ import pandas as pd
 
 def getBasicLessonsData() :
 
-    data = pd.read_excel('API\data\ШК2_расписание_дети с 25.11.2024.xlsx')
+    data = pd.read_excel('./API\data\ШК2_расписание_дети с 25.11.2024.xlsx')
 
     basicColumns = data.columns.tolist()[1::8]
 
-    data.columns =  [data.columns.tolist()[0]] + [col for _ in range(8) for col in basicColumns]
+    data.columns =  [data.columns.tolist()[0]] + list(zip([col for col in basicColumns for _ in range(8)], data.iloc[0, 1:]))
+
+    data.drop(0, inplace=True)
+
+    data.head()
 
     return data
