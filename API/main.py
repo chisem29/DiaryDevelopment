949df823c84be1c16c218dd30dff51f6bcf9f302
samChisem29 <tonyfhg1000@gmail.com>
2024-12-23 19:58:49 +0300

    ndarray

--- API/main.py
+++ API/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def getBasicLessonsData() :
 
@@ -14,6 +15,6 @@ def getBasicLessonsData() :
 
     return data
 
-def getLessonsDataByClassWeekday(data : pd.DataFrame, className : str, weekday : str) :
+def getLessonsDataByClassWeekday(data : pd.DataFrame, className : str, weekday : str) -> np.ndarray:
 
     return data.loc[className, [(weekday, n) for n in range(1, 9)]].dropna().str.split('\n').apply(pd.Series).rename(columns={0 : 'Предмет', 1 : 'Кабинет'}).values
