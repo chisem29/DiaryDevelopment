ec6d977a822ad8d15da647e129ad5bbbaef3e12f
samChisem29 <tonyfhg1000@gmail.com>
2024-12-23 21:32:23 +0300

    DATA CHANGING

--- API/getBasicLessonsData.py
+++ API/getBasicLessonsData.py
@@ -14,7 +14,3 @@ def getBasicLessonsData() -> pd.DataFrame :
     data.set_index('Класс', inplace=True)
 
     return data
-
-def getLessonsDataByClassWeekday(data : pd.DataFrame, className : str, weekday : str) -> np.ndarray :
-
-    return data.loc[className, [(weekday, n) for n in range(1, 9)]].dropna().str.split('\n').apply(pd.Series).rename(columns={0 : 'Предмет', 1 : 'Кабинет'}).values
---
+++ API/tableData.py
@@ -0,0 +1,3 @@
+from getBasicLessonsData import getBasicLessonsData as GBLD
+
+DATA = GBLD() # Наши результирующие данные!!!! Отсюда импорт!!!!!!!!!!!
