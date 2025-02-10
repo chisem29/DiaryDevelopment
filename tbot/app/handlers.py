7fc23bccabe09e02fb193da2f2c75eabaf1b01e4
chisem29 <tonyfhg1000@gmail.com>
2025-02-10 22:00:58 +0300

    change handlers

--- tbot/app/handlers.py
+++ tbot/app/handlers.py
@@ -12,20 +12,21 @@ def setup_handlers(router: Router, data):
 
     @router.message(Command('start'))
     async def cmd_start(message: Message):
-        start_keyboard = await kb.start_button()
-   
-        await message.answer(f"Привет, {message.from_user.first_name}! Чтобы начать, нажмите на кнопку ниже.", reply_markup=start_keyboard)
+        state = await router.fsm.get_state(message.from_user.id)
+        if state:
+            await message.answer("Вы уже начали процесс. Для продолжения выберите класс.")
+        else:
+            start_keyboard = await kb.start_button()
+            await message.answer(f"Привет, {message.from_user.first_name}! Чтобы начать, нажмите на кнопку ниже.", reply_markup=start_keyboard)
 
     @router.callback_query(lambda c: c.data == "start_button")
     async def on_start_button_click(callback_query: CallbackQuery, state: FSMContext):
-
         classes_number_keyboard = await kb.inline_number_classes()
         await callback_query.message.answer(f"Выберите класс:", reply_markup=classes_number_keyboard)
 
     @router.callback_query(lambda c: c.data.startswith('class_number_'))
     async def process_class_number_selection(callback_query: CallbackQuery, state: FSMContext):
         selected_class_number = callback_query.data.split('_')[2]
-
         await state.update_data(selected_class_number=selected_class_number)
         
         class_char_keyboard = await kb.inline_char_classes(selected_class_number)
@@ -38,7 +39,6 @@ def setup_handlers(router: Router, data):
     @router.callback_query(lambda c: c.data.startswith('class_char_'))
     async def process_class_char_selection(callback_query: CallbackQuery, state: FSMContext):
         selected_class_char = callback_query.data.split('_')[2]
-
         await state.update_data(selected_class_char=selected_class_char)
 
         weekdays_keyboard = await kb.inline_weekdays()
@@ -51,7 +51,6 @@ def setup_handlers(router: Router, data):
     @router.callback_query(lambda c: c.data.startswith('weekday_'))
     async def process_weekday_selection(callback_query: CallbackQuery, state: FSMContext):
         selected_weekday = callback_query.data.split('_')[1]
-
         await state.update_data(selected_weekday=selected_weekday)
         
         user_data = await state.get_data()
@@ -77,6 +76,3 @@ def setup_handlers(router: Router, data):
                 await callback_query.message.answer('Ой! Произошла ошибка при получении данных.')
         else:
             await callback_query.message.answer('Ой! Не указаны все данные.')
-
-        await state.storage.close()
-
